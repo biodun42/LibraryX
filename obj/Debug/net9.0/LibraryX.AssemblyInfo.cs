ee187528fa3ec6261ac4538f6351e91d171c569d
Biodun <muhammedabiodun42@gmail.com>
2025-06-01 08:20:00 +0100

    commit

--- obj/Debug/net9.0/LibraryX.AssemblyInfo.cs
+++ obj/Debug/net9.0/LibraryX.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryX")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4692ffb0f7a49ce901ab1ab20bfd953ed3794f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdb3445648e1ed105501a08f53370adc025875cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryX")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryX")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
